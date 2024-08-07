e1477861aa1d7775ab770bfbf2a1170987ce3e6f
Leah Fenster <leahefen@gmail.com>
2024-08-07 19:49:01 +0100

    Aug7 7:48PM edits

--- mini-project-react/src/components/Footer.jsx
+++ mini-project-react/src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <div className="footer">
       <a
-        href="https://github.com/l-fenster/React-App-Mini-Project/tree/main/mini-project-react"
+        href="https://github.com/l-fenster/Jecipes-Recipes-Mini-React-Project"
         target="_blank"
       >
         Click here to go to the repo!
